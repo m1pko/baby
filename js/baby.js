eb401252c44adc878d9074811aa00365b2e038c0
Miguel Barba <Miguel@ndn.home>
2017-04-04 00:02:51 +0100

    Added more logic
    Using more functions to test the moments.js lib

--- js/baby.js
+++ js/baby.js
@@ -1,26 +1,39 @@
 moment.locale('pt');
 
-var now = moment().format('dddd, D MMMM  YYYY');
+//Global variables
 
-var day = moment('2017-02-17');
+var dayOne = moment([2017,01,17]);
 
-var strBeginning = '20170217';
+var beginning = moment('2017-02-17').format('dddd, D MMMM YYYY');
 
-var beginning = day.format('dddd, D MMMM YYYY');
+var dueDateLowerLimit = moment('2017-11-17').format('D MMMM YYYY');
 
-var dueDate = moment().format('dddd, D MMMM YYYY');
+var dueDateUpperLimit = moment('2017-11-24').format('D MMMM YYYY');
 
 function thisMoment()
 {
-	return now;
+	return moment().format('dddd, D MMMM YYYY');
 }
 
 function whenItAllStarted()
 {
-	return window.beginning;
+	return beginning;
 }
 
 function untilNow()
 {
-  return moment(window.now).diff(window.beginning, 'months', true);
+	var timeSoFar = moment.duration(moment().diff(dayOne));
+	return timeSoFar.months() + ' meses e ' + timeSoFar.days() + ' dias.';
 }
+
+function timeToReachMilestone(milestone)
+{
+	var timeTothreeMonths = moment.duration((dayOne.add(milestone,'M')).diff(moment()));
+	return 'Faltam ' + timeTothreeMonths.months() + ' meses e ' + timeTothreeMonths.days() + ' dias para os ' + milestone + ' meses.';
+}
+
+function dueDate()
+{
+	return 'Deverá nascer entre ' + dueDateLowerLimit + ' e ' + dueDateUpperLimit + '.';
+}
+
