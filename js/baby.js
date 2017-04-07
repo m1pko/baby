bc9a96cb3e3a9f8279be569141086d104f3cc8f2
Miguel Barba <Miguel@ndn.home>
2017-04-07 23:11:28 +0100

    Still naked but even more stable

--- js/baby.js
+++ js/baby.js
@@ -1,16 +1,22 @@
+//Regional settings
+
 moment.locale('pt');
 
 //Global variables
 
+var stDayOne = "2017-02-17";
+
 var now = moment();
 
-var dayOne = moment("2017-02-17");
+var dayOne = moment(stDayOne);
 
-var beginning = moment('2017-02-17').format('dddd, D MMMM YYYY');
+var beginning = moment(stDayOne).format('dddd, D MMMM YYYY');
 
 var dueDateLowerLimit = moment('2017-11-17').format('D MMMM YYYY');
 
 var dueDateUpperLimit = moment('2017-11-24').format('D MMMM YYYY');
+
+//Functions
 
 function thisMoment()
 {
@@ -32,44 +38,45 @@ function untilNow()
 function isThereAnyMilestoneToday()
 {
 	var mileStoneArray = new Array();
-	for (var i = 0; i < 9; i++)
+	for (var i = 0; i < 8; i++)
 	{
-		mileStoneArray[i] = moment("2017-02-07").add(i,'M');
+		mileStoneArray[i] = moment(stDayOne).add(i,'M');
 	}
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 8; i++)
 	{
 		if (mileStoneArray[i].isSame(moment().format('MM-DD-YYYY')))
 		{
 			switch(i) 
 			{
-
 			    case 0:
-			        return 'Faz hoje ' + ((i + 1)) + ' meses que tudo começou!';
+			        return 'Faz hoje ' + i + ' meses que tudo começou!';
 			    case 1:
-			        return 'Faz hoje ' + (i + 1) + ' meses que tudo começou!';
+			        return 'Faz hoje ' + i + ' meses que tudo começou!';
 			    case 2:
-			        return 'Faz hoje ' + (i + 1) + ' meses que tudo começou!';
+			        return 'Faz hoje ' + i + ' meses que tudo começou!';
 			    case 3:
-			        return 'Faz hoje ' + (i + 1) + ' meses que tudo começou!';
+			        return 'Faz hoje ' + i + ' meses que tudo começou!';
 			    case 4:
-			        return 'Faz hoje ' + (i + 1) + ' meses que tudo começou!';
+			        return 'Faz hoje ' + i + ' meses que tudo começou!';
 			    case 5:
-			        return 'Faz hoje ' + (i + 1) + ' meses que tudo começou!';
+			        return 'Faz hoje ' + i + ' meses que tudo começou!';
 			    case 6:
-			        return 'Faz hoje ' + (i + 1) + ' meses que tudo começou!';
+			        return 'Faz hoje ' + i + ' meses que tudo começou!';
 			    case 7:
-			        return 'Faz hoje ' + (i + 1) + ' meses que tudo começou!';
+			        return 'Faz hoje ' + i + ' meses que tudo começou!';
 			}
 		}
 	}
+
+	//else
+	return 'Hoje não há nenhuma data a assinalar!';
 
 }
 
 function timeToReachMilestone(milestone)
 {
-	//var untilMilestone = moment.duration(moment([2017,01,17]).add(milestone,'M').format('D MMMM YYYY').diff(now));
-	var untilMilestone = moment.duration((moment([2017,01,17]).add(milestone,'M')).diff(now));
+	var untilMilestone = moment.duration((moment(stDayOne).add(milestone,'M')).diff(now));
 	return 'Faltam ' + untilMilestone.months() + ' meses e ' + untilMilestone.days() + ' dias para os ' + milestone + ' meses.';
 }
 
